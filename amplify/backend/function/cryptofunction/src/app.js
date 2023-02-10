01bc7b8fbe6abbd40987b88a2dc68c63982f2607
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-10 17:43:36 -0600

    adding a call to coinlore api

--- amplify/backend/function/cryptofunction/src/app.js
+++ amplify/backend/function/cryptofunction/src/app.js
@@ -25,16 +25,27 @@ app.use(function(req, res, next) {
   next()
 });
 
-// Creating the coins route using the textbook code
+
+// Import axios
+const axios = require('axios')
+
 app.get('/coins', function(req, res) {
-  const coins = [
-    { name: 'Bitcoin', symbol: 'BTC', price_usd: "10000" },
-    { name: 'Ethereum', symbol: 'ETH', price_usd: "400" },
-    { name: 'Litecoin', symbol: 'LTC', price_usd: "150" }
-  ]
-  res.json({
-    coins
-  })
+  // Define base url
+  let apiUrl = `https://api.coinlore.com/api/tickers?start=0&limit=10`
+
+  // Check if there are any query string parameters
+  // If so, reset the base url to include them
+  if (req.apiGateway && req.apiGateway.event.queryStringParameters) {
+   const { start = 0, limit = 10 } = req.apiGateway.event.queryStringParameters
+   apiUrl = `https://api.coinlore.com/api/tickers/?start=${start}&limit=${limit}`
+  }
+
+  // Call API and return response
+  axios.get(apiUrl)
+    .then(response => {
+      res.json({ coins: response.data.data })
+    })
+    .catch(err => res.json({ error: err }))
 })
 
 /**********************
