6a8227ffe89a0fc4cc6ed9152af3af4be58eb290
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-10 17:26:26 -0600

    changed amplify import to named import
    removed react import unnecessary

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { API } from 'aws-amplify'
 import './App.css';
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
