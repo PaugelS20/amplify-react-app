aa7363f8c9cdb85d75cf1b679a12c44ab9a3045c
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-10 17:22:48 -0600

    Added new textbook code to call api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const App = () => {
+    // Create coins variable and set to empty array
+    const [coins, updateCoins] = useState([]);
+
+    // Define function to all API
+    async function fetchCoins() {
+      const data = await API.get('cryptoapi', '/coins')
+      updateCoins(data.coins);
+    }
+  
+    // Call fetchCoins function when component loads
+    useEffect(() => {
+      fetchCoins()
+    }, [])
+  
+    return (
+      <div className="App">
+        {
+          coins.map((coin, index) => (
+            <div key={index}>
+              <h2>{coin.name} - {coin.symbol}</h2>
+              <h5>${coin.price_usd}</h5>
+            </div>
+          ))
+        };
+      </div>
+    );
+}
 export default App;
